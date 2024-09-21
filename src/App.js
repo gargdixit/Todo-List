1d8bf957b14faba533696c1ee4fa4db6a97a6425
gargdixit <gargdixit9050@gmail.com>
2024-09-21 21:26:05 +0530

    indendation change

--- src/App.js
+++ src/App.js
@@ -21,39 +21,37 @@ function App() {
 
 	return (
 		<div className='px-5'>
-		<h1>Create Task</h1>
-		<div style={{marginTop: '0px'}}>
-			<input type="text" value={input} onChange={handleInputChange} placeholder="Add a new task" />
-			<button onClick={handleAddTodo} className='btn btn-primary' >Add</button>
-		</div>
-		<div className='row'>
-			<div className='col-lg-4 ' >
-				<div className='status p-3 '>
-					<h4 className='text-start border-bottom border-3 border-dark mb-4'>To Do</h4>
-					{todoTickets.map((todoTicket)=>(
-						<div className='tickets'>{todoTicket}</div>
-					))}
+			<h1>Create Task</h1>
+			<div style={{marginTop: '0px'}}>
+				<input type="text" value={input} onChange={handleInputChange} placeholder="Add a new task" />
+				<button onClick={handleAddTodo} className='btn btn-primary' >Add</button>
+			</div>
+			<div className='row'>
+				<div className='col-lg-4 ' >
+					<div className='status p-3 '>
+						<h4 className='text-start border-bottom border-3 border-dark mb-4'>To Do</h4>
+						{todoTickets.map((todoTicket)=>(
+							<div className='tickets'>{todoTicket}</div>
+						))}
+					</div>
+				</div>
+				<div className='col-lg-4' >
+					<div className='status p-3'>
+						<h4 className='text-start border-bottom border-3 border-dark mb-4'>In Progress</h4>
+						{inProgressTickets.map((inProgressTicket)=>(
+							<div className='tickets'>{inProgressTicket}</div>
+						))}
+					</div>
+				</div>
+				<div className='col-lg-4 ' >
+					<div className='status p-3'>
+						<h4 className='text-start border-bottom border-3 border-dark mb-4'>Done</h4>
+						{doneTickets.map((doneTicket)=>(
+							<div className='tickets'>{doneTicket}</div>
+						))}
+					</div>
 				</div>
 			</div>
-			<div className='col-lg-4' >
-				<div className='status p-3'>
-					<h4 className='text-start border-bottom border-3 border-dark mb-4'>In Progress</h4>
-					{inProgressTickets.map((inProgressTicket)=>(
-						<div className='tickets'>{inProgressTicket}</div>
-					))}
-				</div>
-			</div>
-			<div className='col-lg-4 ' >
-				<div className='status p-3'>
-					<h4 className='text-start border-bottom border-3 border-dark mb-4'>Done</h4>
-					{doneTickets.map((doneTicket)=>(
-						<div className='tickets'>{doneTicket}</div>
-					))}
-				</div>
-			</div>
-		</div>
-		
-		
 		</div>
 	);
 }
