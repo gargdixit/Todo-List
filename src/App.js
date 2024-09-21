43496ead1814efb45f8839dc2773d7c1ac82e8b6
dixit <gargdixit9050@gmail.com>
2024-09-21 18:46:50 +0530

    First commit from local branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todoTickets, setTodos] = useState([]);
+	const [inProgressTickets, setInProgress] = useState([]);
+	const [doneTickets, setDone] = useState([]);
+
+  	const [input, setInput] = useState('');
+
+	let handleInputChange = (e)=>{
+		setInput(e.target.value);
+	}
+
+	const handleAddTodo = () => {
+		if (input.trim() !== '') {
+		  setTodos([...todoTickets, input]);
+		  setInput('');
+		}
+	  };
+
+	return (
+		<div className='px-5'>
+		<h1>Create Task</h1>
+		<div style={{marginTop: '0px'}}>
+			<input type="text" value={input} onChange={handleInputChange} placeholder="Add a new task" />
+			<button onClick={handleAddTodo} className='btn btn-primary' >Add</button>
+		</div>
+		<div className='row'>
+			<div className='col-lg-4 ' >
+				<div className='status p-3 '>
+					<h4 className='text-start border-bottom border-3 border-dark mb-4'>To Do</h4>
+					{todoTickets.map((todoTicket)=>(
+						<div className='tickets'>{todoTicket}</div>
+					))}
+				</div>
+			</div>
+			<div className='col-lg-4' >
+				<div className='status p-3'>
+					<h4 className='text-start border-bottom border-3 border-dark mb-4'>In Progress</h4>
+					{inProgressTickets.map((inProgressTicket)=>(
+						<div className='tickets'>{inProgressTicket}</div>
+					))}
+				</div>
+			</div>
+			<div className='col-lg-4 ' >
+				<div className='status p-3'>
+					<h4 className='text-start border-bottom border-3 border-dark mb-4'>Done</h4>
+					{doneTickets.map((doneTicket)=>(
+						<div className='tickets'>{doneTicket}</div>
+					))}
+				</div>
+			</div>
+		</div>
+		
+		
+		</div>
+	);
 }
 
 export default App;
