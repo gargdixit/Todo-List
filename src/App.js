98078780d27be1f79f4f5d254d69b1c19cc7c28a
gargdixit <gargdixit9050@gmail.com>
2024-09-24 22:08:43 +0530

    Add drag and drop functionality

--- src/App.js
+++ src/App.js
@@ -17,7 +17,37 @@ function App() {
 		  setTodos([...todoTickets, input]);
 		  setInput('');
 		}
-	  };
+	};
+
+	const handleDragStart = (e, ticket, from) => {
+		e.dataTransfer.setData('text/plain', JSON.stringify({ ticket, from }));
+	};
+
+	const handleDrop = (e, to) => {
+		e.preventDefault();
+		const { ticket, from } = JSON.parse(e.dataTransfer.getData('text/plain'));
+		if (from !== to) moveTicket(ticket, from, to);
+	};
+
+	const allowDrop = (e) => {
+		e.preventDefault();
+	};
+
+	const moveTicket = (ticket, from, to) => {
+		if (to === 'todo') {
+			if (from === 'inProgress') setInProgress(inProgressTickets.filter(t => t !== ticket));
+			if (from === 'done') setDone(doneTickets.filter(t => t !== ticket));
+			setTodos([...todoTickets, ticket]);
+		} else if (to === 'inProgress') {
+			if (from === 'todo') setTodos(todoTickets.filter(t => t !== ticket));
+			if (from === 'done') setDone(doneTickets.filter(t => t !== ticket));
+			setInProgress([...inProgressTickets, ticket]);
+		} else if (to === 'done') {
+			if (from === 'todo') setTodos(todoTickets.filter(t => t !== ticket));
+			if (from === 'inProgress') setInProgress(inProgressTickets.filter(t => t !== ticket));
+			setDone([...doneTickets, ticket]);
+		}
+	};
 
 	return (
 		<div className='px-5'>
@@ -28,26 +58,26 @@ function App() {
 			</div>
 			<div className='row'>
 				<div className='col-lg-4 ' >
-					<div className='status p-3 '>
-						<h4 className='text-start border-bottom border-3 border-dark mb-4'>To Do</h4>
+					<div className='status p-3 ' onDrop={(e) => handleDrop(e, 'todo')} onDragOver={allowDrop}>
+						 <h4 className='text-start border-bottom border-3 border-dark mb-4'>To Do</h4>
 						{todoTickets.map((todoTicket)=>(
-							<div className='tickets'>{todoTicket}</div>
+							<div draggable="true" className='tickets' onDragStart={(e) => handleDragStart(e, todoTicket, 'todo')}>{todoTicket}</div>
 						))}
 					</div>
 				</div>
 				<div className='col-lg-4' >
-					<div className='status p-3'>
+					<div className='status p-3' onDrop={(e) => handleDrop(e, 'inProgress')} onDragOver={allowDrop}>
 						<h4 className='text-start border-bottom border-3 border-dark mb-4'>In Progress</h4>
 						{inProgressTickets.map((inProgressTicket)=>(
-							<div className='tickets'>{inProgressTicket}</div>
+							<div draggable="true" className='tickets' onDragStart={(e) => handleDragStart(e, inProgressTicket, 'inProgress')}>{inProgressTicket}</div>
 						))}
 					</div>
 				</div>
 				<div className='col-lg-4 ' >
-					<div className='status p-3'>
+					<div className='status p-3' onDrop={(e) => handleDrop(e, 'done')} onDragOver={allowDrop}>
 						<h4 className='text-start border-bottom border-3 border-dark mb-4'>Done</h4>
 						{doneTickets.map((doneTicket)=>(
-							<div className='tickets'>{doneTicket}</div>
+							<div draggable="true" className='tickets' onDragStart={(e) => handleDragStart(e, doneTicket, 'done')}>{doneTicket}</div>
 						))}
 					</div>
 				</div>
